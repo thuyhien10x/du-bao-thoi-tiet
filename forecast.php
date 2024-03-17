8fe73e59e5311a19f8ffd4cf12609bee1c46b4eb
Quan Le Van <quanlv@yopaz.vn>
2024-03-17 12:17:47 +0700

    html

---
+++ forecast.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://api.weatherapi.com/v1/forecast.json?key=a95639a088604a2589c111655241003&q=HaYYYYYYYYY%20Noi&aqi=no&days=6',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
